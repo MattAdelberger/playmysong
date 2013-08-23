1d4c1bc3aa51142873db61e3853c64f3625cf36f
Kelvin Snijman <kelvin@treelined.net>
2013-08-23 18:17:40 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Playmysong::Application.config.secret_key_base = '1db45fb9392f2ceed142b2378666869f8abb7bb2c59b082a75f1983da99c1b401dca7934344789c50cecb36a3d26b6ba2ce04f9cbe87ac9a203ddca7d0f23586'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Playmysong::Application.config.session_store :cookie_store, key: '_playmysong_session'

255d2e7708be376a92d7a9432317b928f77bf81a
Kelvin Snijman <kelvin@treelined.net>
2013-08-24 18:09:10 -0500

    Run rake db:migrate for AudienceMember scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130824223425) do
+ActiveRecord::Schema.define(version: 20130824230434) do
+
+  create_table "audience_members", force: true do |t|
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "events", force: true do |t|
     t.string   "code"

65936f31a4de71cce504ec948170e3b4b7acf330
Kelvin Snijman <kelvin@treelined.net>
2013-08-24 18:44:41 -0500

    Add vote associations

--- app/models/audience_member.rb
+++ app/models/audience_member.rb
@@ -1,2 +1,3 @@
 class AudienceMember < ActiveRecord::Base
+  has_many :votes, dependent: :destroy
 end
--- app/models/set_item.rb
+++ app/models/set_item.rb
@@ -1,4 +1,5 @@
 class SetItem < ActiveRecord::Base
   belongs_to :song
   belongs_to :event
+  has_many :votes, dependent: :destroy
 end

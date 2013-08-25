88c11e2ddd843de5c3652509057ca2dfb25b0b44
Matt Adelberger <matt.adelberger@gmail.com>
2013-08-24 19:50:41 -0500

    Restyled the whole application!

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -46,7 +46,7 @@ class EventsController < ApplicationController
     @event.user_id = current_user.id
     respond_to do |format|
       if @event.save
-        format.html { redirect_to @event, notice: 'Event was successfully created.' }
+        format.html { redirect_to events_path, notice: 'Event was successfully created.' }
         format.json { render action: 'show', status: :created, location: @event }
       else
         format.html { render action: 'new' }
